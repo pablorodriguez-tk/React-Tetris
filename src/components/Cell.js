fa9691b97d85d032e4fb45a57e7341d4181ba16d
pablorodriguez-tk <pablorodriguez88@gmail.com>
2021-07-26 20:28:30 -0300

    Add tetris and cell component styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { TETROMINOS } from '../tetrominos';
+import { StyledCell } from './styles/StyleCell';
 
 const Cell = ({ type }) => {
-  return <div>Cell</div>;
+  return (
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
+      Cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
